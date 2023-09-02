201988ac2cce90470df99f59e373f902db57e292
caresle <carabesle@gmail.com>
2023-09-01 18:33:07 -0600

    Add jwt encrypt and decrypt token

--- backend/src/helpers/jwt.helpers.ts
+++ backend/src/helpers/jwt.helpers.ts
@@ -1,1 +1,20 @@
 import jwt from 'jsonwebtoken';
+import Environment from '../environment';
+
+interface IJWTUserInfo {
+	username: string;
+	rights: number[];
+	rol: string;
+}
+
+export const createJWT = (payload: IJWTUserInfo) => {
+	return jwt.sign(payload, Environment.JWT_SECRET);
+};
+
+export const validateJWT = (token: string) : IJWTUserInfo | undefined => {
+	const tokenValidated = jwt.verify(token, Environment.JWT_SECRET);
+
+	if (!tokenValidated) return undefined;
+
+	return tokenValidated as IJWTUserInfo;
+};
