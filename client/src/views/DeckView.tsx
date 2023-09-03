98fe0f213589e3932c070180902e6f3d65568dda
caresle <carabesle@gmail.com>
2023-09-03 17:11:01 -0600

    Start with deck view

--- client/src/views/DeckView.tsx
+++ client/src/views/DeckView.tsx
@@ -1,8 +1,22 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import {
+	DeckAction
+} from '../components/deck/deck';
+import {
+	onOpenModal,
+	onCloseModal,
+	onOpenChangeModal,
+} from '../helpers/modalControls.helpers'
 
 const DeckView : FC = () => {
+	const [ showCreate, setShowCreate ] = useState(false);
+
 	return (
-		<h1>Hello </h1>
+		<div className='p-10 max-w-5xl mx-auto'>
+			<DeckAction onOpen={() => onOpenModal(setShowCreate)} />
+
+
+		</div>
 	);
 };
 
